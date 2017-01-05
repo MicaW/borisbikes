7e90fb5e4bd54219211901e2b1e5b45ea7b6807b
Shereen Finney <shereenfinney@gmail.com>
2017-01-05 15:52:34 +0000

    Station can dock bike

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,7 +1,23 @@
 require_relative 'bike.rb'
 
 class DockingStation
+
+  def initialize(original_bike=0)
+    @stations_bike = original_bike
+  end
+
+attr_reader :stations_bike
+
+
+
+
   def release_bike
     Bike.new
   end
+
+  def dock_the_bike(bike)
+    @stations_bike = bike
+  end
+
+
 end
